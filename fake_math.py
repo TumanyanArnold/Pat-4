765bc533fc3269d89830ee05567e82d4c7f37ae2
TumanyanArnold <tumanyan2015@list.ru>
2024-09-26 23:50:51 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide (first, second):
+    if second != 0:
+        i = first / second
+        return i
+    else:
+        return 'Ошибка'
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide (first, second):
+    if second != 0:
+        d = first / second
+        return d
+    else:
+        return inf
